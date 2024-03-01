a43e0b17ad239f893c2663944db77d5b3ef00016
mrmamun12 <alabdullah7062@gmail.com>
2024-03-01 17:38:12 +0600

    load data

---
+++ app.js
@@ -0,0 +1,8 @@
+const loadData =async() =>{
+    const url = 'https://openapi.programming-hero.com/api/ai/tools';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+};
+
+loadData();
